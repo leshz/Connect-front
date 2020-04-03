a443f6628df3fe237d4d5ff6571a8ef961d44161
Jeff Barragan <leshz@me.com>
2020-04-02 19:45:27 -0500

    fox

--- src/components/styles.tsx
+++ src/components/styles.tsx
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 
 const H1 = styled.h1`
   text-align: center;
-  font-size:50px;
-  background:red;
+  font-size: 50px;
+  background: red;
+  background-color: blue;
 `;
-
 
 export { H1 };
