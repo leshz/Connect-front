4b726cf3c1696fff452fcb3f852a8a2663406db9
Jeff Barragan <leshz@me.com>
2020-05-07 22:14:41 -0500

    fix

--- src/global-styles.tsx
+++ src/global-styles.tsx
@@ -1,10 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;700&display=swap');
   *{
     margin:0;
-    padding:0;
+    padding:0; 
     box-sizing:border-box;
     font-weight: normal;
   }
